af7576cf930c95ebd062b32fb357a69fbdfaefd8
ZKZOV <izaacsoaresjr@gmail.com>
2023-05-27 13:07:22 -0300

    main.c ateração

---
+++ matrizes/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrizes.h"
+
+int main () {
+
+print_name();
+teste_transposta();
+teste_conjugada();
+teste_hermitiana();
+teste_soma();
+teste_subtracao();
+teste_produtoEscalar();
+teste_produtoMatricial();
+
+return 0;
+}
